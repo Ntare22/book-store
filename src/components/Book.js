6eb777574c3346b2b89ea132786702e547442a9d
Jim Ntare <jntare@Jims-BK-MacBook-Pro.local>
2021-12-16 23:03:08 +0200

    setup redux
    create add and remove book functionalities

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,15 +1,26 @@
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { removeBook } from '../redux/books/books';
 
-const Book = ({ genre, title, author }) => (
-  <div>
-    <p id="genre">{genre}</p>
-    <h2 id="title">{title}</h2>
-    <p id="author">{author}</p>
-    <button type="button">Remove</button>
-  </div>
-);
+const Book = ({
+  id, genre, title, author,
+}) => {
+  const dispatch = useDispatch();
+  const deleteBook = (book) => {
+    dispatch(removeBook(book));
+  };
+  return (
+    <div>
+      <p id="genre">{genre}</p>
+      <h2 id="title">{title}</h2>
+      <p id="author">{author}</p>
+      <button type="button" onClick={() => deleteBook(id)}>Remove</button>
+    </div>
+  );
+};
 
 Book.propTypes = {
+  id: PropTypes.string.isRequired,
   genre: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   author: PropTypes.string.isRequired,
